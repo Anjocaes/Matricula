3453bd242487ff86df1f9372c2aeb8db92828fe7
mark532 <mmurillo532@gmail.com>
2017-02-19 22:00:48 -0600

    Andres(no vamos a usar es6)

--- src/matricula/modelo/Carrera.java
+++ src/matricula/modelo/Carrera.java
@@ -7,6 +7,30 @@ public class Carrera {
         this.titulo = titulo;
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
    String codigo;
    String nombre;
    String titulo;
