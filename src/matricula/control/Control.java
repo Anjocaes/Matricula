6bab00e5c29266f924b4aad2065e366bedcda38c
mark532 <mmurillo532@gmail.com>
2017-02-20 22:03:33 -0600

    Modelo y control (v1)
    puse los  metodos de algunas clases segun la funcionalidad minima del
    profe
    mñn lo sigo me dio suelo y sino no me levanto mñn

--- src/matricula/control/Control.java
+++ src/matricula/control/Control.java
@@ -1,5 +1,11 @@
 package matricula.control;
 
+import matricula.modelo.Modelo;
+
 public class Control {
-    
+   public Control(){
+       datos=new Modelo();
+   }
+   
+    Modelo datos;
 }
--- src/matricula/modelo/Modelo.java
+++ src/matricula/modelo/Modelo.java
@@ -8,6 +8,61 @@ public class Modelo {
                     "Bachillerato y salida lateral de Diplomado en Prgramacion de Aplicaciones Infimaticas");
             
     }
+    //----------------Metodos de carrera-------------------------------------
+    public void agregarCurso(String codigo,String nombre,int cred, int h_s){
+        carrera.crearCurso(codigo, nombre, cred, h_s);
+    }
+    public void eliminarCurso(){
+        
+    }
+    public void busqNom(String nombre){
+        
+    }
+     public void busqCod(String codigo){
+        
+    }
+    //-----------------------Curso-------------------------------------------
+    public void busqCursoNom(String nom){
+        
+    }
+    public void busqCursoCod(String cod){
+        
+    }
+    public void busqCursoCarrera(String carr){
+        
+    }
+    public void addCurso(){
+         
+    }
+    public void deleteCurso(){
+         
+    }
+    public void OrdenarCurso(){
+         
+    }
+    //----------------------Grupo--------------------------------------------
+    
+    //--------------------Profesor-------------------------------------------
+    public void busqProfNom(String nom){
+        
+    }
+    public void busqProfCed(String ced){
+        
+    }
+    //--------------------Alumno---------------------------------------------
+    public void busqAlumNom(String nom){
+        
+    }
+    public void busqAlumCod(String cod){
+        
+    }
+    public void busqAlumCarr(String carr){
+        
+    }
+    public void historial(){
+        
+    }
+    //-----------------------------------------------------------------------
     
     Universidad universidad;
     Carrera carrera;
