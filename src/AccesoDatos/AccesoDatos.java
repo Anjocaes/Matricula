b6bdbb2549bd8d0d9cbe6553cc595ce7afbf04e2
Anjocaes <anjocaes@gmail.com>
2017-03-02 01:21:17 -0600

    accceso
    prof,matri,admin

--- src/AccesoDatos/AccesoDatos.java
+++ src/AccesoDatos/AccesoDatos.java
@@ -3,17 +3,39 @@ package AccesoDatos;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import matricula.conexion.Conector;
+import matricula.modelo.Administrativo;
 import matricula.modelo.Alumno;
+import matricula.modelo.Matriculador;
+import matricula.modelo.Profesor;
 
 public class AccesoDatos {
     public AccesoDatos(){
         connect = new Conector(null,null,null);
     }
     public void addAlumno(Alumno a)throws Exception{
-        String sql = "insert into persona(Cedula, Nombre, Telefono, Correo, Clave, Tipo, Fecha_Nacimiento)values ('%s','%s','%s','%s','%s','%s','%s')";
-        sql=String.format(sql,a.getCedula(),a.getNombre(),a.getTelefono(),a.getCorreo(),a.getClave(),a.getTipo(),a.getFechaNacimiento());
+        String sql = "insert into persona(Cedula, Nombre, Telefono, Correo, Clave, Tipo, Fecha_Nacimiento)values ('%s','%s','%s','%s','%s',0,'%s')";
+        sql=String.format(sql,a.getCedula(),a.getNombre(),a.getTelefono(),a.getCorreo(),a.getClave(),a.getFechaNacimiento());
         int rs = connect.executeUpdate(sql);       
     }
+    
+    public void addProfesor(Profesor a)throws Exception{
+        String sql = "insert into persona(Cedula, Nombre, Telefono, Correo, Clave, Tipo)values ('%s','%s','%s','%s','%s',1,'')";
+        sql=String.format(sql,a.getCedula(),a.getNombre(),a.getTelefono(),a.getCorreo(),a.getClave());
+        int rs = connect.executeUpdate(sql);       
+    }
+    
+     public void addAdministrativo(Administrativo a)throws Exception{
+        String sql = "insert into persona(Cedula, Nombre, Telefono, Correo, Clave, Tipo)values ('%s','%s','%s','%s','%s',2,'')";
+        sql=String.format(sql,a.getCedula(),a.getNombre(),a.getTelefono(),a.getCorreo(),a.getClave());
+        int rs = connect.executeUpdate(sql);       
+    }
+     
+     public void addMatriculador(Matriculador a)throws Exception{
+        String sql = "insert into persona(Cedula, Nombre, Telefono, Correo, Clave, Tipo)values ('%s','%s','%s','%s','%s',3,'')";
+        sql=String.format(sql,a.getCedula(),a.getNombre(),a.getTelefono(),a.getCorreo(),a.getClave(),a.getTipo());
+        int rs = connect.executeUpdate(sql);       
+    }     
+     
     private final Conector connect;
     
 }
--- src/matricula/modelo/Matriculador.java
+++ src/matricula/modelo/Matriculador.java
@@ -1,8 +1,7 @@
 package matricula.modelo;
 public class Matriculador extends Persona{  
     
-    public Matriculador(String cedula, String nombre, String telefono, 
-            String correo, String clave) {
+    public Matriculador(String cedula, String nombre, String telefono, String correo, String clave) {
         super(cedula, nombre, telefono, correo, clave);
     }
 }
